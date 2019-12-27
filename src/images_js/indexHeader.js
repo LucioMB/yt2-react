bf718368fd7cd98a71f088ade84885208043b0d3
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-27 14:30:16 +0100

    Update indexHeader.js

--- src/images_js/indexHeader.js
+++ src/images_js/indexHeader.js
@@ -1,9 +1,9 @@
-export const Header1 = require('./header-1.png');
-export const Header2 = require('./header-2.png');
-export const Logo = require('./header-1.png');
-export const IconSearch = require('./icon-search-p.png');
-export const Header3 = require('./header-3.png');
-export const Header4 = require('./header-4.png');
-export const Header5 = require('./header-5.png');
-export const Header6 = require('./header-6.png');
+export const Header1 = require('../images/header-1.png');
+export const Header2 = require('../images/header-2.png');
+export const Logo = require('../images/header-1.png');
+export const IconSearch = require('../images/icon-search-p.png');
+export const Header3 = require('../images/header-3.png');
+export const Header4 = require('../images/header-4.png');
+export const Header5 = require('../images/header-5.png');
+export const Header6 = require('../images/header-6.png');
 
