ab0ee5bfc20e537d38703a76572f533c99d44f2c
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-27 14:14:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import Header from './header.js';
 import ArticleBar from './article-bar.js';
 import NavBar from './nav-bar.js';
