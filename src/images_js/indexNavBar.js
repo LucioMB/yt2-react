54a83585bb27066ede6de9ab4680dace687a43d9
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-27 14:31:21 +0100

    Update indexNavBar.js

--- src/images_js/indexNavBar.js
+++ src/images_js/indexNavBar.js
@@ -1,29 +1,29 @@
-export const Home = require('./home.png');
-export const Tendenze = require('./tendenze.png');
-export const Iscrizioni = require('./iscrizioni.png');
+export const Home = require('../images/home.png');
+export const Tendenze = require('../images/tendenze.png');
+export const Iscrizioni = require('../images/iscrizioni.png');
 
-export const Raccolta = require('./raccolta.png');
-export const Cronologia = require('./cronologia.png');
-export const GuardoPiuTardi = require('./guardo-piu-tardi.png');
-export const VideoPiaciuti = require('./video-piaciuti.png');
-export const Playlist = require('./playlist.png');
+export const Raccolta = require('../images/raccolta.png');
+export const Cronologia = require('../images/cronologia.png');
+export const GuardoPiuTardi = require('../images/guardo-piu-tardi.png');
+export const VideoPiaciuti = require('../images/video-piaciuti.png');
+export const Playlist = require('../images/playlist.png');
 
-export const Iscrizione1 = require('./iscrizione-1.png');
-export const Iscrizione2 = require('./iscrizione-2.png');
-export const Iscrizione3 = require('./iscrizione-3.png');
-export const Iscrizione4 = require('./iscrizione-4.png');
-export const Iscrizione5 = require('./iscrizione-5.png');
-export const Iscrizione6 = require('./iscrizione-6.png');
-export const Iscrizione7 = require('./iscrizione-7.png');
-export const Iscrizione8 = require('./iscrizione-8.png');
-export const MostraMeno = require('./mostra-meno.png');
+export const Iscrizione1 = require('../images/iscrizione-1.png');
+export const Iscrizione2 = require('../images/iscrizione-2.png');
+export const Iscrizione3 = require('../images/iscrizione-3.png');
+export const Iscrizione4 = require('../images/iscrizione-4.png');
+export const Iscrizione5 = require('../images/iscrizione-5.png');
+export const Iscrizione6 = require('../images/iscrizione-6.png');
+export const Iscrizione7 = require('../images/iscrizione-7.png');
+export const Iscrizione8 = require('../images/iscrizione-8.png');
+export const MostraMeno = require('../images/mostra-meno.png');
 
-export const Premium = require('./premium.png');
-export const Film = require('./film.png');
-export const Videogiochi = require('./videogiochi.png');
-export const DalVivo = require('./dal-vivo.png');
+export const Premium = require('../images/premium.png');
+export const Film = require('../images/film.png');
+export const Videogiochi = require('../images/videogiochi.png');
+export const DalVivo = require('../images/dal-vivo.png');
 
-export const Impostazioni = require('./impostazioni.png');
-export const Segnalazioni = require('./segnalazioni.png');
-export const Guida = require('./guida.png');
-export const Feedback = require('./feedback.png');
+export const Impostazioni = require('../images/impostazioni.png');
+export const Segnalazioni = require('../images/segnalazioni.png');
+export const Guida = require('../images/guida.png');
+export const Feedback = require('../images/feedback.png');
