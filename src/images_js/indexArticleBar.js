6755d933e31844587cfd79ea1696684c1b321113
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-27 14:29:42 +0100

    Update indexArticleBar.js

--- src/images_js/indexArticleBar.js
+++ src/images_js/indexArticleBar.js
@@ -1,8 +1,8 @@
-export const Iscrizione1 = require('./iscrizione-1.png');
-export const Iscrizione2 = require('./iscrizione-2.png');
-export const Iscrizione3 = require('./iscrizione-3.png');
-export const Iscrizione4 = require('./iscrizione-4.png');
-export const Iscrizione5 = require('./iscrizione-5.png');
-export const Iscrizione6 = require('./iscrizione-6.png');
-export const Iscrizione7 = require('./iscrizione-7.png');
-export const Iscrizione8 = require('./iscrizione-8.png');
+export const Iscrizione1 = require('../images/iscrizione-1.png');
+export const Iscrizione2 = require('../images/iscrizione-2.png');
+export const Iscrizione3 = require('../images/iscrizione-3.png');
+export const Iscrizione4 = require('../images/iscrizione-4.png');
+export const Iscrizione5 = require('../images/iscrizione-5.png');
+export const Iscrizione6 = require('../images/iscrizione-6.png');
+export const Iscrizione7 = require('../images/iscrizione-7.png');
+export const Iscrizione8 = require('../images/iscrizione-8.png');
