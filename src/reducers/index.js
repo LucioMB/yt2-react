d5c344db8aa24120e65818073c26b6e03fb81732
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-29 19:00:40 +0100

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,8 @@ import { combineReducers } from "redux";
 
 import videos from "./videos";
 
-export default combineReducers({ videos });
+import navBarLink from "./nav_bar";
+
+import headerSearch from "./header";
+
+export default combineReducers({ videos, navBarLink, headerSearch });
